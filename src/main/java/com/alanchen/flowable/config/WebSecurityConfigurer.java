fd13dfdf04d6877efa08e218e60d2c2c9a2ae466
AlanChen <chenyan900520@126.com>
2020-10-16 10:48:08 +0800

    权限

---
+++ src/main/java/com/alanchen/flowable/config/WebSecurityConfigurer.java
@@ -0,0 +1,28 @@
+package com.alanchen.flowable.config;
+
+import com.smm.framework.authority.GlobalWebSecurityConfigurer;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+/**
+ * @author Alan Chen
+ * @description Spring security配置
+ * @date 2019-10-12
+ */
+@Configuration
+@Order(SecurityProperties.BASIC_AUTH_ORDER)
+public class WebSecurityConfigurer extends GlobalWebSecurityConfigurer {
+
+
+    @Override
+    protected HttpSecurity customConfigure(HttpSecurity http) throws Exception {
+
+        http.cors().and().csrf().disable().authorizeRequests()
+                .antMatchers("/expense/**").permitAll()
+                .anyRequest().authenticated();
+        return http;
+    }
+
+}
